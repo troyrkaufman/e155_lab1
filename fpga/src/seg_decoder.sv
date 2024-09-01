5d90b063997b37c16245608b659507759e0c5ab2
Troy Kaufman <tkaufman@hmc.edu>
2024-09-01 16:50:16 -0700

    "Fixed_seven_seg_decoder"

--- fpga/src/seg_decoder.sv
+++ fpga/src/seg_decoder.sv
@@ -8,23 +8,23 @@ module seg_decoder (input logic [3:0] s,
     //Properly decode the seven segement display
     always_comb
         case (s)
-            0:      seg = 7'b000_0001;
-            1:      seg = 7'b100_1111; 
-            2:      seg = 7'b001_0010;
-            3:      seg = 7'b000_0110;
-            4:      seg = 7'b100_1100;
-            5:      seg = 7'b010_0100;
-            6:      seg = 7'b010_0000;
-            7:      seg = 7'b000_1110;
+            0:      seg = 7'b100_0000;
+            1:      seg = 7'b111_1001; 
+            2:      seg = 7'b010_0100;
+            3:      seg = 7'b011_0000;
+            4:      seg = 7'b001_1001;
+            5:      seg = 7'b001_0010;
+            6:      seg = 7'b000_0010;
+            7:      seg = 7'b111_1000;
             8:      seg = 7'b000_0000;
-            9:      seg = 7'b000_1100;
+            9:      seg = 7'b001_1000;
             10:     seg = 7'b000_1000;
-            11:     seg = 7'b110_0000;
-            12:     seg = 7'b011_0001;
-            13:     seg = 7'b100_0010;
-            14:     seg = 7'b011_0000;
-            15:     seg = 7'b011_1000;
-            default: seg = 7'b000_0001;
+            11:     seg = 7'b000_0011;
+            12:     seg = 7'b100_0110;
+            13:     seg = 7'b010_0001;
+            14:     seg = 7'b000_0110;
+            15:     seg = 7'b000_1110;
+            default: seg = 7'b100_0000;
         endcase
 
 endmodule
